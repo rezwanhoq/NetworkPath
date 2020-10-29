72732d97d713ee3cef12ca53b01531da0e2a8163
Rezwan <rezwanhoq@hotmail.com>
2020-10-29 17:23:03 +1100

    algorithm prepared

---
+++ src/NetwrokPath.php
@@ -0,0 +1,132 @@
+<?php
+
+class NetworkPath
+{
+    public $graph;
+
+    public $distance;
+
+    public $previous;
+
+    public $queue;
+
+    /**
+     * @param integer[][] $graph
+     */
+    public function __construct($graph)
+    {
+        $this->graph = $graph;
+    }
+
+    /**
+     * Process the next (i.e. closest) entry in the queue.
+     *
+     * @param string[] $exclude A list of nodes to exclude - for calculating next-shortest paths.
+     *
+     * @return void
+     */
+    public function processNextNodeInQueue(array $exclude)
+    {
+        // Process the closest vertex
+        $closest = array_search(min($this->queue), $this->queue);
+        if (!empty($this->graph[$closest]) && !in_array($closest, $exclude)) {
+            foreach ($this->graph[$closest] as $neighbor => $cost) {
+                if (isset($this->distance[$neighbor])) {
+                    if ($this->distance[$closest] + $cost < $this->distance[$neighbor]) {
+                        // A shorter path was found
+                        $this->distance[$neighbor] = $this->distance[$closest] + $cost;
+                        $this->previous[$neighbor] = array($closest);
+                        $this->queue[$neighbor] = $this->distance[$neighbor];
+                    } elseif ($this->distance[$closest] + $cost === $this->distance[$neighbor]) {
+                        // An equally short path was found
+                        $this->previous[$neighbor][] = $closest;
+                        $this->queue[$neighbor] = $this->distance[$neighbor];
+                        
+                    }
+                }
+            }
+        }
+        unset($this->queue[$closest]);
+        
+    }
+
+    /**
+     * Extract all the paths from $source to $target as arrays of nodes.
+     *
+     * @param string $target The starting node (working backwards)
+     *
+     * @return string[][] One or more shortest paths, each represented by a list of nodes
+     */
+    public function extractPaths($target)
+    {
+        $paths = array(array($target));
+
+        for ($key = 0; isset($paths[$key]); ++$key) {
+            $path = $paths[$key];
+
+            if (!empty($this->previous[$path[0]])) {
+                foreach ($this->previous[$path[0]] as $previous) {
+                    $copy = $path;
+                    array_unshift($copy, $previous);
+                    $paths[] = $copy;
+                }
+                unset($paths[$key]);
+            }
+        }
+
+        return array_values($paths);
+    }
+
+    /**
+     * Calculate the shortest path through a a graph, from $source to $target.
+     *
+     * @param string   $source  The starting node
+     * @param string   $target  The ending node
+     * @param string[] $exclude A list of nodes to exclude - for calculating next-shortest paths.
+     *
+     * @return string[][] Zero or more shortest paths, each represented by a list of nodes
+     */
+    public function shortestPaths($source, $target, array $exclude = array())
+    {
+        // The shortest distance to all nodes starts with infinity...
+        $this->distance = array_fill_keys(array_keys($this->graph), INF);
+        // ...except the start node
+        $this->distance[$source] = 0;
+
+        // The previously visited nodes
+        $this->previous = array_fill_keys(array_keys($this->graph), array());
+
+        // Process all nodes in order
+        $this->queue = array($source => 0);
+        while (!empty($this->queue)) {
+            $this->processNextNodeInQueue($exclude);
+        }
+
+        if ($source === $target) {
+            // A null path
+            return array(array($source));
+        } elseif (empty($this->previous[$target])) {
+            // No path between $source and $target
+            return array();
+        } else {
+            // One or more paths were found between $source and $target
+            return $this->extractPaths($target);
+        }
+    }
+}
+
+
+    $graph = array(
+        'A' => array('B' => 10, 'C' => 20),
+        'B' => array('A' => 10, 'D' => 100),
+        'C' => array('A' => 20, 'D' => 30),
+        'D' => array('B' => 100, 'C' => 30, 'E' => 10),
+        'E' => array('D' => 10, 'F' => 1000),
+        'F' => array('E' => 1000),
+        'G' => array(),
+      ); 
+    
+    $algorithm = new NetworkPath($graph); 
+    $path = $algorithm->shortestPaths('D', 'A');
+    echo $algorithm->distance['A'];
+    var_dump($path);
